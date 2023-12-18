637ff3dda8767adb87d2a187731d7944ed5157c4
Mahadev Sagi <madhsa1972@gmail.com>
2023-12-17 21:37:13 -0500

    last message

---
+++ tuplesetlist/liststuplessets.py
@@ -0,0 +1,11 @@
+# lists
+courses = ['ap calc', 'ap gov', 'ap environmental', 'hcc classes', 'teachers assistant']
+
+# This will correctly print the length of the courses list
+print(len(courses))
+
+# This will correctly print the entire list
+print(courses)
+
+# To print the second element (index 1, as indexing starts at 0) of the courses list
+print(courses[1])
